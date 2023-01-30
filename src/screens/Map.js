55b53eb2e664b8e01ec711e0fe9edd38b0d7a793
Akib Ahmed <akibahmed@iut-dhaka.edu>
2023-01-30 23:53:39 +0200

    Map added

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,93 +1,178 @@
-/*import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-export default function Map() {
-  return (
-    <View>
-      <Text>Map</Text>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({})*/
-
-import React, {useEffect, useState} from 'react';
+import MapView, { LatLng, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import {
   StyleSheet,
-  Text,
   View,
-  Pressable,
-  Alert,
-  TextInput
-} from 'react-native';
-import CustomButton from '../utils/CustomButton';
-import GlobalStyle from '../utils/GlobalStyle';
-import SQLite from 'react-native-sqlite-storage';
+  Dimensions,
+  Text,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+import {
+  GooglePlaceDetail,
+  GooglePlacesAutocomplete,
+} from "react-native-google-places-autocomplete";
+import { useRef, useState } from "react";
+import MapViewDirections from "react-native-maps-directions";
+import { getPreciseDistance} from 'geolib';
 
-const db = SQLite.openDatabase(
-  {
-      name: 'MainDB',
-      location: 'default',
-  },
-  () => {},
-  error => {console.log(error)},
-);
+const GOOGLE_API_KEY = "YOUR_API_KEY";
 
-export default function Map({ navigation }) {
+const { width, height } = Dimensions.get("window");
 
-  const [id, setID] = useState('');
-  const [vehicle, setVehicle] = useState("");
-  const [car, setCar] = useState("");
-  const [fuel, setFuel] = useState("");
-  const [distance, setDistance] = useState('');
-  const [iname, setIName] = useState("");
-  const [emission, setEmission] = useState('');
-  const [date, setDate] = useState();
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = () => {
-    try {
-        db.transaction((tx) => {
-            tx.executeSql(
-                "SELECT ID, Vehicle, Vehicle_Type, Fuel, Distance, Date, Category, Emission FROM Trips WHERE ID=1",
-                [],
-                (tx, results) => {
-                    var len = results.rows.length;
-                    if (len > 0) {
-                        var DB_id = results.rows.item(0).ID;
-                        var userName = results.rows.item(0).Vehicle;
-                        var userAge = results.rows.item(0).Distance;
-                        var db_category = results.rows.item(0).Category;
-                        var vehicleType = results.rows.item(0).Vehicle_Type;
-                        var fuel = results.rows.item(0).Fuel;
-                        var dbDate = results.rows.item(0).Date;
-                        var DBemission = results.rows.item(0).Emission;
-
-                        setID(DB_id);
-                        setVehicle(userName);
-                        setDistance(userAge);
-                        setIName(db_category);
-                        setFuel(fuel);
-                        setCar(vehicleType);
-                        setDate(dbDate);
-                        setEmission(DBemission);
-                    }
-                }
-            )
-        })
-    } catch (error) {
-        console.log(error);
-    }
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+  latitude: 61.06499650000001,
+  longitude: 28.0943387,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
 };
 
+
+export default function App({ navigation }) {
+
+  const [distance, setDistance] = useState();
+  const [origin, setOrigin] = useState();
+  const [destination, setDestination] = useState();
+  let origin_lat, origin_long, dest_lat, dest_long;
+  const mapRef = useRef(null);
+
+  const moveTo = async (position) => {
+    const camera = await mapRef.current.getCamera();
+    if (camera) {
+      camera.center = position;
+      mapRef.current.animateCamera(camera, { duration: 1000 });
+    }
+  };
+
+  
+  const onPlaceSelected = (details, flag) => {
+    const position = {
+      latitude: details?.geometry.location.lat || 0,
+      longitude: details?.geometry.location.lng || 0,
+    };
+    moveTo(position);
+    //console.log(position);
+    //console.log(details.address_components[0].long_name);
+
+    if (flag === 'origin') {
+      origin_lat = position.latitude;
+      origin_long = position.longitude;
+      setOrigin(details.address_components[0].long_name);
+    } 
+    if (flag === 'destination') {
+      dest_lat = position.latitude;
+      dest_long = position.longitude;
+      setDestination(details.address_components[0].long_name);
+    }
+
+  };
+
+  const calculatePreciseDistance = () => {
+    try {
+      var pdis = getPreciseDistance(
+        {latitude: origin_lat, longitude: origin_long},
+        {latitude: dest_lat, longitude: dest_long},
+      );
+      setDistance(pdis/1000);
+      // console.log(distance);
+
+    } catch (error) {
+      console.log(error);
+    }
+    
+  };
+
   return (
-    <View>
-      <Text>{id}, {vehicle}, {car}, {fuel}, {distance}, {iname}, {date}, {emission}</Text>
+    <View style={styles.container}>
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_POSITION}
+      >
+          <MapViewDirections
+            apikey={GOOGLE_API_KEY}
+            strokeColor="#6644ff"
+            strokeWidth={4}
+          />
+      </MapView>
+      <View style={styles.searchContainer}>
+        <GooglePlacesAutocomplete
+          styles={{ textInput: styles.input }}
+          placeholder="Origin"
+          fetchDetails
+          onPress={(data, details = null) => {
+            onPlaceSelected(details, 'origin');
+          }}
+          query={{
+            key: GOOGLE_API_KEY,
+            language: "pt-BR",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          styles={{ textInput: styles.input }}
+          placeholder="Destination"
+          fetchDetails
+          onPress={(data, details = null) => {
+            onPlaceSelected(details, 'destination');
+          }}
+          query={{
+            key: GOOGLE_API_KEY,
+            language: "pt-BR",
+          }}
+        />
+        <TouchableOpacity style={styles.button} onPress={calculatePreciseDistance}>
+          <Text style={styles.buttonText}>Total Distance</Text>
+        </TouchableOpacity>
+        <Text>{distance} KM</Text>
+        { distance > 0 && 
+        <Button title="OK" onPress={() => navigation.navigate('Trip' ,{
+          distance: distance,
+          origin: origin,
+          destination: destination
+        })} />
+        }
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  searchContainer: {
+    position: "absolute",
+    width: "90%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 8,
+    borderRadius: 8,
+  },
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
+  },
+  button: {
+    backgroundColor: "#bbb",
+    paddingVertical: 12,
+    marginTop: 16,
+    borderRadius: 4,
+  },
+  buttonText: {
+    textAlign: "center",
+  },
+});
